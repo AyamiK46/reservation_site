79232bb121507021b5591017da4a3d56cf2e0276
kawada <ayami0406pop@gmail.com>
2022-02-21 17:07:42 +0900

    reservation/showのcontroller &viewを修正してエラー解消

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,26 +2,26 @@ class ReservationsController < ApplicationController
   def index
     @reservations = current_user.reservations
   end
-
+  
   def new
     @room = Room.find(params[:id])
     @reservation = Reservation.new(reservation_params)
     render "rooms/show" if @reservation.invalid?
   end
-
+  
   def create 
     @room = Room.find(params[:id])
     @reservation = Reservation.new(reservation_params)
     render "rooms/show" and return if params[:back] || !@reservation.save
     redirect_to @reservation
   end
-
+  
   def show
-    @room = Room.find_by(id: params[:id])
+    @reservation = Reservation.find_by(id: params[:id])
   end
-
+  
   private
-
+  
   def reservation_params
     params.require(:reservation).permit(:start, :end_date, :guests, :fee, :total, :total_day).merge(user_id: current_user.id, room_id: @room.id)
   end
